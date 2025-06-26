cc554c8c4e7b3e8e53e98da647c9753bdbecf1c5
Monicah Koina <monicahkoina1@gmail.com>
2025-06-27 00:15:12 +0300

    connect to mySQL

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mysql = require('mysql2')
 const port = 3001;
 app.use(cors());
 app.use(express.json())
@@ -9,4 +11,18 @@ app.get("/", (req, res) => {
 })
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
+})
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+
+})
+db.connect((err) => {
+    if (err) {
+        console.log("Database connection failed", err.stack)
+        return;
+    } console.log("Connected to mysql")
 })
